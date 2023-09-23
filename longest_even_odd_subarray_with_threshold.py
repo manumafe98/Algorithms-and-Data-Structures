0563d0662aa359028889ecab3ddeaa73bb774e94
manumafe98 <manumaxerapro@gmail.com>
2023-09-23 14:43:10 -0300

    Adding longest even odd subarray with threshold leetcode exercise

---
+++ longest_even_odd_subarray_with_threshold.py
@@ -0,0 +1,73 @@
+# Leetcode
+"""
+You are given a 0-indexed integer array nums and an integer threshold.
+
+Find the length of the longest subarray of nums starting at index l 
+and ending at index r (0 <= l <= r < nums.length) that satisfies the following conditions:
+
+nums[l] % 2 == 0
+For all indices i in the range [l, r - 1], nums[i] % 2 != nums[i + 1] % 2
+For all indices i in the range [l, r], nums[i] <= threshold
+Return an integer denoting the length of the longest such subarray.
+
+Note: A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,5,4], threshold = 5
+Output: 3
+Explanation: In this example, we can select the subarray that starts at l = 1 and ends at r = 3 => [2,5,4]. 
+This subarray satisfies the conditions.
+Hence, the answer is the length of the subarray, 3. We can show that 3 is the maximum possible achievable length.
+Example 2:
+
+Input: nums = [1,2], threshold = 2
+Output: 1
+Explanation: In this example, we can select the subarray that starts at l = 1 and ends at r = 1 => [2]. 
+It satisfies all the conditions and we can show that 1 is the maximum possible achievable length.
+Example 3:
+
+Input: nums = [2,3,4,5], threshold = 4
+Output: 3
+Explanation: In this example, we can select the subarray that starts at l = 0 and ends at r = 2 => [2,3,4]. 
+It satisfies all the conditions.
+Hence, the answer is the length of the subarray, 3. We can show that 3 is the maximum possible achievable length.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100 
+1 <= nums[i] <= 100 
+1 <= threshold <= 100
+"""
+from typing import List
+
+class Solution:
+    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+        # Define two variables, current maximum subarray and longest
+        longest = 0 
+        current = 0 
+        for i in range(len(nums)):
+            # First check if the number meets the threshold criteria
+            if nums[i] > threshold: 
+                current = 0 
+            # Then if the subarray wasn't initialized, check for a even number
+            elif current == 0 and nums[i] % 2 == 0: 
+                current = 1 
+            # If the subarray was started check if the previous value meet the condition nums[i] % 2 != nums[i - 1] % 2
+            # For simplicity instead of checking the next one check the previous one
+            elif current > 0 and nums[i] % 2 != nums[i - 1] % 2: 
+                current += 1 
+            # 
+            else: 
+                # A good example to undestand this else is for example nums = [2, 2, 3] threshold = 4
+                # With the first 2, current = 1, the 2dn 2 will not met the criteria, but can start a new valid 
+                # subarray, thats why it checks if its even, and set it to 1 and if not 0
+                current = 1 if nums[i] % 2 == 0 else 0
+            
+            # Get the maximum between longest and current, every iteration
+            # For cases like [2, 8] so longest will store 1 and then fail with 8, but return 1
+            longest = max(longest, current) 
+        return longest
